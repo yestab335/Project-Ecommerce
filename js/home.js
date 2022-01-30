e2c71ba0a4943c213b553c15fc7456fd74949275
Rayan Tabora <superguy723@Rayans-MacBook-Pro.local>
2022-01-29 22:25:18 -0500

    Final Commit

---
+++ js/home.js
@@ -0,0 +1,18 @@
+// Constant Variables
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+// Best Selling Image Slider Buttons
+productContainers.forEach((item, i) => {
+    let containerDimenstions = item.getBoundingClientRect();
+    let containerWidth = containerDimenstions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
